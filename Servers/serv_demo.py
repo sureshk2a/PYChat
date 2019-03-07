70181f5691eb3ae78436c06bdc53f3a6bb9c5112
Suresh <sureshkumar_a@outlook.com>
2019-03-07 15:52:32 +0530

    init

---
+++ Servers/serv_demo.py
@@ -0,0 +1,11 @@
+import socket
+host = socket.gethostbyname(socket.gethostname())
+port = 4000
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+print("Started serveron port: ",port)
+s.listen(1)
+c,addr = s.accept()
+print("Connected from: ",addr)
+s.send(b"HAI")
+s.send("Bye".encode())
