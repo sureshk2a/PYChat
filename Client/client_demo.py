62a5cd157affa381d5c3a21d732cad60836f4d3c
Suresh <sureshkumar_a@outlook.com>
2019-03-07 18:02:44 +0530

    WinError 10057 after connecting to client

--- Client/client_demo.py
+++ Client/client_demo.py
@@ -1,12 +1,14 @@
 import socket
+# ip_to_connect = input("Which IP do you wanna connect to? :")
+# client_s.connect((ip_to_connect,4000))
 client_s = socket.socket()
-#ip_to_connect = input("Which IP do you wanna connect to? :")
-#client_s.connect((ip_to_connect,4000))
-client_s.connect(('localhost',4000))
-msg = client_s.recv(1024)
-#Loop as long as there is a message to receive
-while msg:
-    print("Received",msg.decode())
+def message():
+    client_s.connect(('localhost', 4000))
     msg = client_s.recv(1024)
-client_s.close()
+    while msg:
+        print("Received", msg.decode())
+        msg = client_s.recv(1024)
+    return client_s
+cl_conn = message()
+cl_conn.close()
 
