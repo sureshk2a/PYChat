daa400b54231c86a9f04be895ac4e7aea71b6a9b
Suresh <sureshkumar_a@outlook.com>
2019-03-07 17:48:08 +0530

    WinError 10057 after connecting to client

--- Servers/serv_demo.py
+++ Servers/serv_demo.py
@@ -1,11 +1,13 @@
 import socket
-host = socket.gethostbyname(socket.gethostname())
+#host,port = socket.gethostbyname(socket.gethostname()),4000
+host = 'localhost'
 port = 4000
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print(host)
 s.bind((host,port))
 print("Started serveron port: ",port)
-s.listen(1)
+s.listen(2)
 c,addr = s.accept()
 print("Connected from: ",addr)
-s.send("Bye".encode())
+s.send("Hai im Pewds".encode())
 s.close()
