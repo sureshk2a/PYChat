03bafef8921d918c9bcd25026a197702979fbcab
Suresh <sureshkumar_a@outlook.com>
2019-03-07 17:19:09 +0530

    Added Client demo code

--- Client/client_demo.py
+++ Client/client_demo.py
@@ -1,6 +1,7 @@
 import socket
 client_s = socket.socket()
 msg = client_s.recv(1024)
+#Loop as long as there is a message to receive
 while msg:
     print("Received",msg.decode())
     msg = client_s.recv(1024)
