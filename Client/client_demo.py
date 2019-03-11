28a37ca6134f1a0fa53b2d86224c95085083a762
unknown <venkatesh18596@gmail.com>
2019-03-11 15:54:48 +0530

    Added conversation of chat sekeleton

--- Client/client_demo.py
+++ Client/client_demo.py
@@ -1,14 +1,22 @@
-import socket
+import socket, threading
 # ip_to_connect = input("Which IP do you wanna connect to? :")
 # client_s.connect((ip_to_connect,4000))
 client_s = socket.socket()
+
+def receive_message(client_s):
+    while True:
+        msg = client_s.recv(1024)
+        if len(msg.decode()) > 2:
+            print ("Received", msg.decode())
+
 def message():
     client_s.connect(('localhost', 4000))
-    msg = client_s.recv(1024)
-    while msg:
-        print("Received", msg.decode())
-        msg = client_s.recv(1024)
+    t1 = threading.Thread(target = receive_message, args=(client_s,))
+    t1.start()
+    while True:
+        inp = raw_input();
+        client_s.send(inp)
     return client_s
+
 cl_conn = message()
 cl_conn.close()
-
--- Servers/serv_demo.py
+++ Servers/serv_demo.py
@@ -1,12 +1,25 @@
-import socket
+import socket, time, threading
+
+def receive_message(conn):
+    while True:
+        msg = conn.recv(1024)
+        if len(msg.decode()) > 2:
+            print ("Received", msg.decode())
+            conn.send("Roger That : "+msg.decode())
+            if msg.decode() == "exit":
+                conn.close()
+                threading._shutdown()
+
 #host,port = socket.gethostbyname(socket.gethostname()),4000
 host = 'localhost' #Using localhost addr for now
 port = 4000
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((host,port))
+connections  = {};
 print("Started server on : ",host,":",port)
 s.listen(1)
-c,addr = s.accept()
-print("Connected from: ",addr)
-c.send("Hai im Pewds".encode())
-c.close()
+conn,addr = s.accept()
+conn.send("Welcome...")
+t2 = threading.Thread(target = receive_message, args = (conn,))
+t2.start()
+#conn.close()
