030d6c6ae610b13ba06571230773d16eace348e5
Suresh <sureshkumar_a@outlook.com>
2019-03-15 20:36:33 +0530

    Encoded all the messages to bitsy6

--- Client/client_demo.py
+++ Client/client_demo.py
@@ -1,8 +1,7 @@
 import socket, threading
 # ip_to_connect = input("Which IP do you wanna connect to? :")
 # client_s.connect((ip_to_connect,4000))
-client_s = socket.socket()
-
+client_s = socket.socket()                 #------CLIENT------
 def receive_message(client_s):
     while True:
         msg = client_s.recv(1024)
@@ -11,12 +10,16 @@ def receive_message(client_s):
 
 def message():
     client_s.connect(('localhost', 4000))
-    t1 = threading.Thread(target = receive_message, args=(client_s,))
+    t1 = threading.Thread(target = receive_message(client_s,)).setName("Receive_MSG Thread")
     t1.start()
     while True:
-        inp = raw_input();
-        client_s.send(inp)
+        inp = input();
+        client_s.send(inp.encode())
     return client_s
+try:
+    cl_conn = message()
+except BaseException  as e:
+    print("Message not received: ", e)
+else:
+    cl_conn.close()
 
-cl_conn = message()
-cl_conn.close()
