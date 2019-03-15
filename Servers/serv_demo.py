fccb927e9864125ebcc29695c80b6c22e328c149
Suresh <sureshkumar_a@outlook.com>
2019-03-15 20:42:12 +0530

    Encoded all the messages

--- Servers/serv_demo.py
+++ Servers/serv_demo.py
@@ -1,31 +1,33 @@
 import socket, time, threading
 
-global connections;
+global connections;                            #-----SERVER-------
 threads = {};
 thread_count = 0;
+host,port = 'localhost',4000
 
 def receive_message(conn):
     while True:
         msg = conn.recv(1024)
-        if len(msg.decode()) > 2:
-            print ("Received", msg.decode())
-            conn.send("Roger That : "+msg.decode())
+        while msg:
+            print(msg.decode)
+            conn.send("Received by: ",host)
             if msg.decode() == "exit":
                 conn.close()
                 threading._shutdown()
 
 #host,port = socket.gethostbyname(socket.gethostname()),4000
-host = 'localhost' #Using localhost addr for now
-port = 4000
+
+  #Using localhost addr for now
+
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((host,port))
 connections  = {};
-print("Started server on : ",host,":",port)
+print("Started server on : ",host," : ",port)
 s.listen(3)
 while True:
     conn, addr = s.accept()
-    conn.send("Welcome..." + str(addr[0]) + ":" + str(addr[1]))
-    thread_count = thread_count + 1;
-    threads[thread_count] = threading.Thread(target = receive_message, args = (conn,))
+    conn.send(b"Welcome...: " + str(addr[0]).encode() + b" : " + str(addr[1]).encode())
+    thread_counts = thread_count + 1;
+    threads[thread_count] = threading.Thread(target = receive_message(conn,))
     threads[thread_count].start()
 #conn.close()
